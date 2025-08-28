a34503dcf41b692c79319ed3834ac0f1d5b4cecd
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2025-08-28 19:02:44 -0400

    Add basic Chrome storage get logic for testing

--- src/command-bar/command-bar.jsx
+++ src/command-bar/command-bar.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+const isEmptyObject = (object) => {
+    for (const prop in object) {
+        if (Object.hasOwn(object, prop)) { return false }
+    }
+    return true;
+}
 const parseInput = (input) => input.trim().split(" ").filter((string) => string !== "");
 
 function CommandBar() {
     const [cmdInput, setCmdInput] = useState("");
+    const [commands, setCommands] = useState({});
+
+    useEffect(() => {
+        const getStorage = async (key) => {
+            try {
+                const storage = await chrome.storage.local.get(key);
+                console.log(isEmptyObject(storage));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getStorage("commands");
+    }, [])
 
     return (
         <div id="command-bar">
@@ -12,7 +32,7 @@ function CommandBar() {
                 value={cmdInput}
                 onChange={(event) => setCmdInput(event.target.value)}
             />
-            <button></button>
+            <button>Search</button>
         </div>
     )
 }
