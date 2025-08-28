dea2a85e919e843c901ee85b39e6a575cdc8ce0f
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2025-08-28 18:27:38 -0400

    Add parsing logic

--- src/command-bar/command-bar.jsx
+++ src/command-bar/command-bar.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
+const parseInput = (input) => input.trim().split(" ").filter((string) => string !== "");
+
 function CommandBar() {
+    const [cmdInput, setCmdInput] = useState("");
+
     return (
         <div id="command-bar">
-            <input type="text"></input>
+            <input
+                type="text"
+                value={cmdInput}
+                onChange={(event) => setCmdInput(event.target.value)}
+            />
+            <button></button>
         </div>
     )
 }
