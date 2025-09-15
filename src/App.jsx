4c0542e496329f47c65b73b1a112f1394c0fa2f7
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2025-09-14 22:25:36 -0400

    Change help messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import CommandBar from './command-bar/command-bar'
 import { useState, useRef } from 'react'
-import { HELP_MESSAGE } from './command-bar/commands/help';
 import './App.css'
 
+const INITIAL_MESSAGE = `--- QUICK HELP ---
+
+To search on a website, enter a valid search command into the command bar, followed by the search prompt
+e.g. To search for square watermelons on google.com, enter "g square watermelons" into the command bar
+
+To see a list of the available search commands, enter "sl" into the command bar
+
+For more in-depth instructions, enter "help" into the command bar`;
+
 function App() {
-    const [output, setOutput] = useState(HELP_MESSAGE);
+    const [output, setOutput] = useState(INITIAL_MESSAGE);
 
     const commandBarRef = useRef(null);
     const focusCommandBar = () => { commandBarRef.current.focus() };
--- src/command-bar/commands/help.jsx
+++ src/command-bar/commands/help.jsx
@@ -1,10 +1,12 @@
-const HELP_MESSAGE = `--- SEARCHING WITH COMMANDS ---
+const HELP_MESSAGE = `--- HELP ---
+
+- SEARCHING WITH COMMANDS -
 To search using the command bar, enter a search command followed by the search prompt into the command bar
 e.g. To search for square watermelons on google.com, enter "g square watermelons" into the command bar
 
 To see the list of available search commands, enter "sl" into the command bar
 
---- ADDING SEARCH COMMANDS ---
+- ADDING SEARCH COMMANDS -
 To add a custom search command, enter the command "sadd" followed by a command name, the name of the website, and its URL
 e.g. To add youtube.com as a search command, enter "sadd y YouTube https://youtube.com/results?search_query="
 
@@ -12,7 +14,7 @@ To delete a search command, enter "sdel" followed by the name of the command int
 
 To reset the list of search commands to the default values, enter "sreset" into the command bar
 
---- HELP COMMAND ---
+- HELP COMMAND -
 Enter "help" into the command bar to see these instructions again
 Enter "cl" into the command bar to see the list of regular commands`;
 
@@ -20,4 +22,4 @@ const help = ({ setOutput }) => {
     setOutput(HELP_MESSAGE);
 }
 
-export { help, HELP_MESSAGE }
+export { help }
